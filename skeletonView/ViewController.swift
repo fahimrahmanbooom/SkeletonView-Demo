d03fab306ff2a24cdcf6c2c37a0db26eec73cf59
circuit <fahimrahmanbooom@gmail.com>
2020-04-19 16:10:28 +0600

    Initial Commit

---
+++ skeletonView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  skeletonView
+//
+//  Created by Fahim Rahman on 19/4/20.
+//  Copyright © 2020 Fahim Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
